4136be2e2eb2d74db75bb683d5e0c7c43d751c95
inquick <309196171@qq.com>
2018-01-30 17:45:06 +0800

    结果显示优化
    因霸结果显示优化

--- yingba/GetReportForm.php
+++ yingba/GetReportForm.php
@@ -39,8 +39,23 @@ if ($result && strlen($result) > 0)
       // echo var_dump($js->data->lst_data->value[i]->date);
       //echo '<br><br>';
       //echo var_dump($js->data->lst_data->value[i]->lst_via->value);
-      echo $js->data->lst_data->value[$i]->date . ' : 记录= <br>';
-      echo var_dump($js->data->lst_data->value[$i]->lst_via->value) . '<br>';
+      echo $js->data->lst_data->value[$i]->date . ' : 记录<br>';
+      for ($j=0; $j < count($js->data->lst_data->value[$i]->lst_via->value); $j++) {
+        echo "游戏名：" . $js->data->lst_data->value[$i]->lst_via->value[$j]->name . '<br>';
+        echo '<td><table border="1">';
+				echo '<td>appid</td><td>appname</td><td>setup_user_num</td><td>login_user_num</td><td>active_user_num</td><td>pay_user_num</td><td>pay_money</td><td>pay_rate</td><td>pay_arppu</td><td>pay_total</td><td>retention_rate_2day</td><td>retention_rate_3day</td><td>retention_rate_7day</td><td>retention_rate_14day</td><td>retention_rate_30day</td><td>retention_rate_60day</td>';
+        echo '<tr>';
+        for ($k=0; $k < count($js->data->lst_data->value[$i]->lst_via->value[$j]->lst_app->value); $k++) {
+          while (list($key, $value) = each($js->data->lst_data->value[$i]->lst_via->value[$j]->lst_app->value[$k])) {
+            if ($key == '_classname') {
+              break;
+            }
+            echo '<td>' . $value . '</td>';
+          }
+          echo '<tr>';
+        }
+        echo '</table></td>';
+      }
     }
   }
   else
